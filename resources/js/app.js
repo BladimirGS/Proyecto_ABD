376b6635876813dad4e9aefadbcf305ea24d6916
BladimirGS <hugobladimirgs@gmail.com>
2024-06-14 19:33:21 -0600

    app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,8 @@
 import './bootstrap';
 import './index';
+
+import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm'
+
+Livewire.start()
+
+import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel-livewire-tables-all.js';
